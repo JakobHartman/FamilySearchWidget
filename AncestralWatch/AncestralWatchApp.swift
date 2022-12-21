915abd5695e0a78754cee5747fd92e85b2757d51
Jakob Hartman <jakobhartman@hotmail.com>
2022-12-20 20:09:44 -0500

    Initial Commit

---
+++ AncestralWatch/AncestralWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AncestralWatchApp.swift
+//  AncestralWatch
+//
+//  Created by Jakob Hartman on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct AncestralWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
